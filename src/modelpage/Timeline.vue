df52c55517ab778cf435d419bad7d6a12d1846ee
Tom Wendland <32841473+iar5@users.noreply.github.com>
2020-06-12 11:35:15 +0200

    timeline showing current time domain, pan is clamped to current time stamp, added control input elements, loading data with end_date and limit

--- src/modelpage/Timeline.vue
+++ src/modelpage/Timeline.vue
@@ -3,7 +3,9 @@
       <div id="chartWrapper"></div>
       <div id="tools">
         <input id="btnPlay" type="button" value="play">
-
+        <input id="btnPin" type="button" value="pin">
+        <input id="btnBrush" type="button" value="brush">
+        <input id="nmbSpeed" type="number" value="1" min=1 max=20>
       </div>
   </div>
 </template>
@@ -37,6 +39,7 @@
 
 <script>
 import Timeline from './timeline/Timeline.js';
+import moment from 'moment';
 
 export default {
   props: ["STORE"],
@@ -61,17 +64,21 @@ export default {
       }
     })
 
-    document.querySelector("#btnPlay").onclick = e => {
-      timeline.isPlaying() ? timeline.pause() : timeline.play()
-    }
+    document.querySelector("#btnPlay").onclick = e => { timeline.isPlaying() ? timeline.pause() : timeline.play() }
+    document.querySelector("#nmbSpeed").oninput = e => { timeline.setSpeed( e.target.value) }
+    document.querySelector("#btnPin").onclick = e => { timeline.setTool("pin") }
+    document.querySelector("#btnBrush").onclick = e => { timeline.setTool("brush") }
   },
   methods: {
     transformData(api_json_data){
-        let data = []
-        for(var d of api_json_data){
-            data.push({date: new Date(d.date), value: d.value})        
-        }  
-        return data
+      let data = []
+      for(var d of api_json_data){
+          data.push({date: new Date(d.date), value: d.value})        
+      }  
+      return data
+    },
+    formatDateForServer(dateTime) {
+      return moment.utc(dateTime, 'MM-DD-YYYY HH:mm:ss').format("YYYY-MM-DD HH:mm:ss");
     },
     async getModelData(id) {
         let response = await fetch(process.env.API_URL + `/models/${id}`)
@@ -80,7 +87,7 @@ export default {
         return response;
     },
     async getSensorData(id) {
-        let response = await fetch(process.env.API_URL + `/sensors/${id}/data`)
+        let response = await fetch(process.env.API_URL + `/sensors/${id}/data?limit=100&end_date=${this.formatDateForServer(new Date())}`)
             .then(res => { return res.json() })
             .catch(err => { throw err });
         return response;
--- src/modelpage/timeline/Timeline.js
+++ src/modelpage/timeline/Timeline.js
@@ -1,33 +1,50 @@
 /**
  * @author Tom Wendland
  */
-import * as d3 from 'd3'
+
 import Graph from "./Graph.js";
+
+import * as d3 from 'd3'
 import { turnArrow } from "../babylon/sensorSelection"
 import { SENSOR_COLORS } from '../../storage/Settings';
 
+//const SENSOR_COLORS = d3.schemeCategory10 // position mapped to sensorId
+
+
+
 const Timeline = (function(parentElement){
+
+    const graphs = new Map()
 
     const width = parentElement.clientWidth // is on 100% width per default
     const height = parentElement.clientHeight
-    const margin = ({top: 20, right: 20, bottom: 20, left: 30}) // used for labels, axis etc
+    const margin = ({top: 20, right: 20, bottom: 22, left: 30}) // used for labels, axis etc
 
-    const svg = d3.create("svg").attr("viewBox", [0, 0, width, height]);
+    const svg = d3.create("svg").attr("viewBox", [0, 0, width, height])
+    .attr("user-select", "none")
     parentElement.appendChild(svg.node())
-
-    const graphs = new Map()
     
-    // scaling reference https://stackoverflow.com/questions/56553384/d3-v5-axis-scale-change-panning-way-too-much
+
+
+
+    /**
+     _____     _     
+    |  _  |_ _|_|___ 
+    |     |_'_| |_ -|
+    |__|__|_,_|_|___|
+    */
+    let _startdate = new Date()
+    _startdate.setTime(_startdate.getTime() - 24*60*60*1000)
+
     const xScaleRef = d3.scaleUtc()
         .range([margin.left, width - margin.right])
-        .domain([new Date("2020-05-22T00:00:00Z"), new Date("2020-05-29T12:00:00Z")]) // TODO scaleTick 24 h
+        .domain([_startdate, new Date()]) // TODO scaleTick use 24 h
 
-    const xScale = xScaleRef.copy()
+    const xScale = xScaleRef.copy() // scaling reference https://stackoverflow.com/questions/56553384/d3-v5-axis-scale-change-panning-way-too-much
 
     const yScale = d3.scaleLinear()
         .range([height - margin.bottom, margin.top])
-        //.domain([0, d3.max(data, d => d.value)]).nice()
-        .domain([0, 50]).nice()
+        .domain([0, 50]).nice() //.domain([0, d3.max(data, d => d.value)]).nice()
 
     const xAxis = g => g
         .attr("transform", `translate(0,${height-margin.bottom})`)
@@ -35,6 +52,8 @@ const Timeline = (function(parentElement){
             .axisBottom(xScale)
             .ticks(width/80)
             .tickSizeOuter(0))
+        .style('cursor', 'ew-resize')
+
 
     const yAxis = g => g
         .attr("transform", `translate(${margin.left},0)`)
@@ -50,7 +69,36 @@ const Timeline = (function(parentElement){
     const gx = svg.append("g").call(xAxis);
     const gy = svg.append("g").call(yAxis);
 
+    svg.selectAll("text")
+    .attr("pointer-events", "none")
 
+
+
+    /**
+     _____               
+    |__   |___ ___ _____ 
+    |   __| . | . |     |
+    |_____|___|___|_|_|_|
+    */
+    let _tExtendStartDate = new Date(2020, 1, 1)
+    const zoom = d3.zoom()
+        .extent([[margin.left, 0], [width-margin.right, height]])
+        .scaleExtent([0.1, 5]) // zoom factor range, depends on preselected domain 
+        .translateExtent([[xScale(_tExtendStartDate)], [xScale(new Date())]]) // pan range
+        .on("zoom", zoomPan)
+
+    svg.call(zoom)
+    .on("dblclick.zoom", null)
+
+    function zoomPan() {  
+        let t = d3.event.transform
+        xScale.domain(t.rescaleX(xScaleRef).domain()); // continous scale with transformed domain  
+
+        replaceTimepin() 
+        gx.call(xAxis); 
+        graphs.forEach(g => g.area(xScale, yScale))        
+        if(selection) brushGroup.call(brush.move, [xScale(selection[0]), xScale(selection[1])]);       
+    }
 
 
 
@@ -61,8 +109,10 @@ const Timeline = (function(parentElement){
       |_| |_|_|_|_|___|  _|_|_|_|
                       |_|        
     */
+    const timepinDate = xScale.domain()[1] // upper domain value
+
     const timepin = svg.append("g")
-    var timepinDate = xScale.domain()[1] // upper domain value
+    .style('cursor', 'ew-resize')
 
     const ruler = timepin.append("line")
         .attr("stroke", "grey")
@@ -79,32 +129,33 @@ const Timeline = (function(parentElement){
 
     timepin.call(d3.drag().on('drag', () => {
         let mouse_x = d3.mouse(svg.node())[0]
+        clampTimelinePosAndSetDate(mouse_x)
+        replaceTimepin()
+    }));
+
+    svg.on("click", function() {         
+        let mouse_x = d3.mouse(this)[0]
+        clampTimelinePosAndSetDate(mouse_x)
+        replaceTimepin()
+    }); 
+
+    function clampTimelinePosAndSetDate(mouse_x){
         let max_x = width-margin.right
         let min_x = margin.left
         if(mouse_x > max_x) mouse_x = max_x
         if(mouse_x < min_x) mouse_x = min_x
-        timepinDate = xScale.invert(mouse_x)        
-        replaceTimepin()
-    }));
+        timepinDate.setTime(xScale.invert(mouse_x))        
+    }
 
-    /*svg.on("mouseup", function() { 
-        let mouse_x = d3.mouse(this)[0]
-        let max_x = width-margin.right
-        if(mouse_x > max_x) mouse_x = max_x
-        date = x.invert(mouse_x)
-        replaceTimepin(date)
-    });*/
-    
     function replaceTimepin(){   
+        //console.log(timepinDate);   
         //let stringdate = formatDate(timepinDate)
         timepin.attr("transform", `translate(${xScale(timepinDate)}, 0)`)
         Array.from(graphs.keys()).forEach(key => {turnArrow(parseInt(key.slice(9)), graphs.get(key).gradient(xScale, yScale, timepinDate))})
     }
-
     replaceTimepin() 
 
     
-
 
     /**                 
      _____             _   
@@ -128,13 +179,14 @@ const Timeline = (function(parentElement){
             }
         })
 
+
     const brushGroup = svg.append("g")
         .call(brush)
         .on("dblclick", () => {
             brushGroup.call(brush.move, null); 
             // brush.end event is called afterwards which removes the selection      
-        });
-        
+        })
+
     brushGroup.select(".selection")
         .attr("fill", "lightgrey")     
         .attr("stroke", "grey")     
@@ -144,58 +196,45 @@ const Timeline = (function(parentElement){
 
 
     /**
-     _____               
-    |__   |___ ___ _____ 
-    |   __| . | . |     |
-    |_____|___|___|_|_|_|
-    */
-
-    // rechts un links icon zum ziehen
-    // oben mitte ein zum draggen
-    // erstellen/entfernen mit doppelklick und button
-    // -> normale pan control ermöglichen
-
-    svg.call(d3.zoom()
-        .scaleExtent([1, 20]) // zoom factor range depending on preselected domain size
-        .extent([[margin.left, 0], [width-margin.right, height]])
-        //.translateExtent([[margin.left, -Infinity], [width - margin.right, Infinity]]) // pan range
-        .on("zoom", zoomed))
-        .on("dblclick.zoom", null)
-
-    function zoomed() {
-        let t = d3.event.transform
-        xScale.domain(t.rescaleX(xScaleRef).domain()); // continous scale with transformed domain  
-
-        replaceTimepin() 
-        gx.call(xAxis); 
-        graphs.forEach(g => g.area(xScale, yScale))        
-        if(selection) brushGroup.call(brush.move, [xScale(selection[0]), xScale(selection[1])]);       
-    }
-
-
-    /**
      _____ _         
     |  _  | |___ _ _ 
     |   __| | .'| | |
     |__|  |_|__,|_  |
                 |___|
     */
-
+    var speed = 1
     var playing = false
-    function animate(){
-        requestAnimationFrame(animate)
-        if(!playing) return
 
-        let m = 1
-        timepinDate.setTime(timepinDate.getTime() + (m*60*1000));
 
-        if(selection){
-            if(timepinDate < selection[0] || timepinDate > selection[1])
-                timepinDate.setTime(selection[0].getTime())
+
+    /**                    
+     __ __       _     _       
+    |  |  |___ _| |___| |_ ___ 
+    |  |  | . | . | .'|  _| -_|
+    |_____|  _|___|__,|_| |___|
+          |_|                  
+     */
+    function update() {
+        requestAnimationFrame(update)
+
+        // TODO fetch new data
+        //zoom.translateExtent([[xScale(_tExtendStartDate)], [xScale(Date.now())]]);
+
+        if(playing){
+            timepinDate.setTime(timepinDate.getTime() + (speed*60*1000));
+
+            if(selection){
+                if(timepinDate < selection[0] || timepinDate > selection[1])
+                    timepinDate.setTime(selection[0].getTime())
+            }
+
+            if(timepinDate > Date.now())
+                timepinDate.setTime(Date.now())
+
+            replaceTimepin()
         }
-        replaceTimepin()
     }
-    requestAnimationFrame(animate)
+    requestAnimationFrame(update)
 
 
     
@@ -215,6 +254,8 @@ const Timeline = (function(parentElement){
      * @param {Number} id a id which you want the graph to be identified with. Use the id for additional actions e.g. removing the graph
      */
     function plotGraph(data, id){
+        console.log(data);
+        
         //console.log(data[0], data[data.length-1]);
         let color = SENSOR_COLORS[id]
         let graph = new Graph(svg, data, color)
@@ -226,8 +267,6 @@ const Timeline = (function(parentElement){
 
         if(graphs.has(idstr)) throw new Error(`Graph with id ${id} already exists`)
         graphs.set(idstr, graph)
-
-        replaceTimepin()
     }
 
     /**
@@ -247,18 +286,46 @@ const Timeline = (function(parentElement){
      * @param {Date} date 
      */
     function setTimepinTime(date){
-        timepinDate = date
+        timepinDate.setTime(date)
         replaceTimepin()
      }
+
+    const _brushMouseDown = brushGroup.on('mousedown.brush');
+    setTool("pin")
+     /**
+     * 
+     * @param {*} tool 
+     */
+    function setTool(tool){
+        console.log(tool);
+        
+        if(tool == "brush") {
+            brushGroup.on('mousedown.brush', _brushMouseDown)
+            brushGroup.select(".handle--w").style('cursor', 'ew-resize')
+            brushGroup.select(".handle--e").style('cursor', 'ew-resize')
+            brushGroup.select(".selection").style('cursor', 'move')
+            svg.select(".overlay").style('cursor', 'crosshair')
+        } else {
+            brushGroup.on('mousedown.brush', () => {})
+            brushGroup.select(".handle--w").style('cursor', 'unset')
+            brushGroup.select(".handle--e").style('cursor', 'unset')
+            brushGroup.select(".selection").style('cursor', 'unset')
+            svg.select(".overlay").style('cursor', 'unset')
+        }
+    }
 
     return {
         plotGraph, 
         removeGraph,  
         setTimepinTime, 
-  
+        setTool,
+
         play(){ playing = true },
         pause(){ playing = false },
         isPlaying(){ return playing },
+
+        getSpeed(){ return speed },
+        setSpeed(s){ speed = s }
     }
 })
 export default Timeline
