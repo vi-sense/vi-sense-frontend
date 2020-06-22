324700e8aec5646944eb668514fcfd813a7cf3af
RomanKrajewski <s0553506@htw-berlin.de>
2020-06-22 19:20:24 +0200

    implement display of past anomalies

--- src/modelpage/History.vue
+++ src/modelpage/History.vue
@@ -1,35 +1,64 @@
 <template lang="html">
-    <div class="history" v-if="model">
-        <h1 class="model__title">{{ model.name }}</h1>
-        <v-card v-for="sensor in model.sensors">
-            <v-card-title :style="`color: ${sensor_colors[sensor.id]}`">{{sensor.name}}</v-card-title>
-        </v-card>
+    <div class="history">
+        <v-list class="overflow-y-auto">
+            <v-card v-for="anomaly in anomalies">
+                <v-container class="pa-0">
+                    <v-row align="center" justify="start" :no-gutters="true">
+                        <v-col-1>
+                            <svg height="50" width="50">
+                                <circle cx="25" cy="25" r="20" stroke="black" stroke-width="3"
+                                        :fill="sensor_colors[anomaly.start_data.sensor_id]"/>
+                            </svg>
+                        </v-col-1>
+                        <v-col>
+                            <v-card-title>
+                                {{`${sensorsById.get(anomaly.start_data.sensor_id).name}: ${anomaly.type}`}}
+                            </v-card-title>
+                            <v-card-subtitle v-if="anomaly.end_data">{{`${anomaly.start_data.date} -
+                                ${anomaly.end_data.date}`}}
+                            </v-card-subtitle>
+                            <v-card-subtitle v-else>{{`${anomaly.start_data.date}`}}</v-card-subtitle>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card>
+        </v-list>
     </div>
 </template>
 
 <script>
-    import axios from "axios";
-
-    import { SENSOR_COLORS } from '../storage/Settings';
+    import {SENSOR_COLORS} from '../storage/Settings';
 
     export default {
         props: ["id"],
         data() {
             return {
                 model: null,
-                endpoint: process.env.API_URL+"/",
+                sensorsById: Map,
+                anomalies: [],
+                endpoint: process.env.API_URL,
                 sensor_colors: SENSOR_COLORS
             };
         },
         methods: {
-            getModel(id) {
-                axios(this.endpoint + "models/"+id)
-                    .then(response => {
-                        this.model = response.data;
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
+            async getModel(id) {
+                try {
+                    const response = await fetch(this.endpoint + "/models/" + id)
+                    this.model = await response.json()
+                } catch (error) {
+                    console.log(error)
+                }
+                this.sensorsById = new Map()
+                this.anomalies = []
+                await Promise.all(this.model.sensors.map(async (sensor) => {
+                    this.sensorsById.set(sensor.id, sensor)
+                    try {
+                        const sensorAnomalies = await fetch(`${this.endpoint}/sensors/${sensor.id}/anomalies`)
+                        this.anomalies.push(...await sensorAnomalies.json())
+                    } catch (error) {
+                        console.log(error)
+                    }
+                }))
             }
         },
         created() {
@@ -44,10 +73,20 @@
 </script>
 
 <style scoped lang="scss">
-    .model {
-        position: relative;
-        max-width: 500px;
-        margin: 0 auto;
-        padding: 50px 20px 70px;
+    .v-card {
+        margin: 5px
     }
+    .v-card__title {
+         font-size: 1rem;
+         line-height: 1rem;
+         word-break: normal;
+         margin-bottom: 10px;
+    }
+    .v-card__subtitle {
+        line-height: 1rem;
+    }
+    .history {
+        overflow: auto;
+    }
+
 </style>
