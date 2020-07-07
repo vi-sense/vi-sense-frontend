07650c8afaa0e2f927bb23b30f02696b13eb9f84
Roman Krajewski <s0553506@htw-berlin.de>
2020-07-07 20:42:37 +0200

    dont show anomalies that are in the future

--- src/modelpage/History.vue
+++ src/modelpage/History.vue
@@ -54,8 +54,9 @@
                 this.anomalies = []
                 await Promise.all(this.modelData.sensors.map(async (sensor) => {
                     this.sensorsById.set(sensor.id, sensor)
+                    const current_date = moment().format("YYYY-MM-DD HH:mm:ss")
                     try {
-                        const sensorAnomalies = await fetch(`${this.endpoint}/sensors/${sensor.id}/anomalies`)
+                        const sensorAnomalies = await fetch(`${this.endpoint}/sensors/${sensor.id}/anomalies?end_date=${current_date}`)
                         this.anomalies.push(...await sensorAnomalies.json())
                     } catch (error) {
                         console.log(error)
