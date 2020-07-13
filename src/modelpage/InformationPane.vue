d529526cd0746f455d8d70093ae8781590c61131
Tom Wendland <32841473+iar5@users.noreply.github.com>
2020-07-13 18:28:57 +0200

    adjusted tooltip styling + added tooltips for timeline icons

--- src/modelpage/InformationPane.vue
+++ src/modelpage/InformationPane.vue
@@ -8,7 +8,7 @@
                     </v-checkbox>
                     <span>{{sensor.name}}</span>
                     <template #actions>
-                    <v-tooltip bottom max-width="20rem">
+                    <v-tooltip bottom max-width="16rem">
                         <template #activator="{ on, attrs }">
                             <v-icon class="px-1"
                                     v-bind="attrs"
@@ -24,7 +24,6 @@
                                     v-on="on"
                             >mdi-arrow-right-circle-outline</v-icon>
                         </template>
-<!--                                    color="rgba(82, 186, 162, 1)"-->
                         <span>Go to Sensor</span>
                     </v-tooltip>
                         <v-tooltip v-else bottom>
