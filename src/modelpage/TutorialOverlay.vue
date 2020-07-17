8c531b033026be0802b589797ed501becc209ee0
lnlytumbleweed <legendary.js@gmail.com>
2020-07-17 09:34:09 +0200

    wording fix

--- src/modelpage/TutorialOverlay.vue
+++ src/modelpage/TutorialOverlay.vue
@@ -9,7 +9,7 @@
         1. Navigate the 3D view using your mouse.
         <br />2. To see a sensor value select a sensor either in 3D view or in the sensors list on the left hand side.
         <br />3. Anomalies indicate errors. Press them to go directly to the sensor.
-        <br />4. The timeline shows the progression of the measured date depending of the selected Sensors.
+        <br />4. The timeline shows the progression of the measured date depending of the selected sensors.
       </div>
     </v-overlay>
   </div>
